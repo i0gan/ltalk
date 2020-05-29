ddc6c9ae93d4b18e443006279bdf044bf445cae7
I0gan <l418894113@gmail.com>
2020-05-29 22:41:22 +0800

    new

---
+++ server/src/net/timer.cc
@@ -0,0 +1,12 @@
+#include "timer.hh"
+Ltalk::Timer::Timer() {
+
+}
+
+Ltalk::Timer::~Timer() {
+
+}
+
+time_t Ltalk::Timer::GetExpiredTime() {
+
+}
--- server/src/net/timer.hh
+++ server/src/net/timer.hh
@@ -7,7 +7,27 @@ class Timer;
 using SPTimer = std::shared_ptr<Timer>;
 
 class Timer {
+public:
+    Timer();
+    ~Timer();
+    Timer(Timer &t);
+    void Update(int timeout);
+    bool IsValid();
+    void ClearRequest();
+    void set_delete();
+    bool IsDelete();
+    time_t GetExpiredTime();
+private:
+    bool deleted_;
+    time_t expired_time_;
+    SPHttp sp_http_;
+};
 
+class TimerCompare {
+public:
+    bool operator()(SPTimer &sp_timer_1, SPTimer &sp_timer_2) const {
+        return sp_timer_1->GetExpiredTime() > sp_timer_2->GetExpiredTime();
+    }
 };
 
 class TimerManager {
