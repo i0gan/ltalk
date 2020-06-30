f375330575213fb1c75deb1abca897ee3c077738
i0gan <l418894113@gmail.com>
2020-06-30 09:15:01 +0800

    new

---
+++ git.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+comment="new"
+cancel="server/build/data/user/ "
+cancel=$cancel"server/build/data/group/ "
+cancel=$cancel"server/build/data/log/ "
+
+if [[ $1 ]]; then
+	comment=$1
+fi
+
+git add .
+git reset HEAD $cancel
+git commit -m "$commit"
+git status
+git push
+
+#echo $cancel
+#echo $comment
