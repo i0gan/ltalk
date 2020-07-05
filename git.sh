e2320fbd69e2ea51dfbc04048d790dcdaf575cab
i0gan <l418894113@gmail.com>
2020-07-05 11:01:50 +0800

    asfasdf

--- git.sh
+++ git.sh
@@ -7,6 +7,7 @@ cancel=$cancel"server/build/data/log/ "
 if [[ $1 ]]; then
 	comment=$1
 fi
+
 # clean obj
 cd server
 make clean
@@ -17,8 +18,9 @@ cd ..
 git add .
 git reset HEAD $cancel
 git commit -m "$comment"
-git status
-git push
+echo $comment
+#git status
+#git push
 
 
 #echo $cancel
